180b0bc139eda73b6764f1b5de0e31093cf9efd2
LBlackmail <leon.blackmail@gmail.com>
2022-08-13 09:19:40 -0400

    fg

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,5 @@
 delay = 3000;
-max_angle = 20;
+max_angle = 0;
 
 console.log(list);
 
@@ -25,12 +25,10 @@ function nextImage()
 	var name_ele = document.getElementById("name");
     name_ele.innerHTML = rand.split(".")[0];
 	img_ele.setAttribute("src", dir+rand);
-    deg = Math.floor(Math.random() * (2*max_angle)) - max_angle;
-    img_ele.style.transform = "rotate("+deg+"deg)";
     /*while(!loaded){console.log(loaded);}
     loaded = false;*/
-    img_ele.style.opacity = "1";
-    img_ele.style.zIndex = "10";
+    img_ele.style.opacity = "0";
+    img_ele.style.zIndex = "0";
 	console.log(img_ele.getAttribute("src"));
     current = other(current);
 	var img_ele = document.getElementById(current);
