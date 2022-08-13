77f0731d2f84e138569e6a9412a193b98629ea78
LBlackmail <leon.blackmail@gmail.com>
2022-08-13 09:25:31 -0400

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -23,17 +23,7 @@ function nextImage()
 	var rand = list[Math.floor(Math.random() * (list.length-1))+1];
 	var img_ele = document.getElementById(current);
 	var name_ele = document.getElementById("name");
-    name_ele.innerHTML = rand.split(".")[0];
-	img_ele.setAttribute("src", dir+rand);
-    /*while(!loaded){console.log(loaded);}
-    loaded = false;*/
-    img_ele.style.opacity = "0";
-    img_ele.style.zIndex = "0";
-	console.log(img_ele.getAttribute("src"));
-    current = other(current);
-	var img_ele = document.getElementById(current);
-    img_ele.style.opacity = "0";
-    img_ele.style.zIndex = "0";
+
 }
 
 nextImage();
